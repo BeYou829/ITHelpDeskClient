4f62c89e5de999469e96e5cfd292c42e2ec4d57d
BeYou <engelcontreras07@gmail.com>
2024-11-05 11:26:26 -0400

    Adding a details page

---
+++ Models/ViewModels/TicketDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace ITHelpDeskClient.Models.ViewModels
+{
+    public class TicketDetails
+    {
+        public AppUser? User { get; set; }
+        public TicketDetailsVM TicketDetailsVM { get; set; } = new TicketDetailsVM();
+    }
+}
---
+++ Models/ViewModels/TicketDetailsVM.cs
@@ -0,0 +1,12 @@
+﻿namespace ITHelpDeskClient.Models.ViewModels
+{
+    public class TicketDetailsVM
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public string RequestType { get; set; } = string.Empty;
+        public DateTime CreatedOn { get; set; }
+        public string RequestNumber { get; set; } = string.Empty;
+    }
+}
