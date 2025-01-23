2bebe1c352a57ad99b67e7c4c2c1acf5e3b7eeea
Engel <engelcontreras07@gmai.com>
2025-01-23 15:54:14 -0400

    Ending the Index

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ITConnection"));
 });
 
 builder.Services.AddIdentity<AppUser,AppRole>(options =>
